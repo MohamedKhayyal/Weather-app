87107f709bb8636a64aee5e705eaaddcc505df71
MohamedKhayyal <khayyalmohamed5@gmail.com>
2025-04-20 17:33:51 +0200

    complete

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,6 +1,34 @@
 import "./Weather.css";
+import search from "../assets/search.png";
+import weather from "../assets/clear.png";
+import humidity from "../assets/humidity.png";
+import wind from "../assets/wind.png";
 export default function Weather() {
-  return <div className="weather">
-    
-  </div>;
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={search} alt="" />
+      </div>
+      <img src={weather} className="weather-icon" />
+      <p className="temp">16°C</p>
+      <p className="city">London</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity} alt="" />
+          <div>
+            <p>90 %</p>
+            <p>Humidity</p>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind} alt="" />
+          <div>
+            <p>3.6 Km/h</p>
+            <p>wind speed</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
